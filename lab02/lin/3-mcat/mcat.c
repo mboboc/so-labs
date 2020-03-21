60934728ebd9a2487bb6203f284a491576cf7556
madalina.boboc <madalina.boboc@stud.acs.upb.ro>
2020-03-22 00:30:35 +0200

    lab2 xread xwrite complete

--- lab02/lin/3-mcat/mcat.c
+++ lab02/lin/3-mcat/mcat.c
@@ -21,12 +21,12 @@
 
 int main(int argc, char **argv)
 {
-	int fd_src, fd_dst, rc = -1, bytesRead = -1;
+	int fd_src, fd_dst, bytesRead = -1;
 	char buffer[BUFSIZE];
 
 	if (argc < 2 || argc > 3) {
 		printf("Usage:\n\t%s source_file [destination_file]\n",
-			argv[0]);
+		       argv[0]);
 		return 0;
 	}
 
@@ -40,21 +40,39 @@ int main(int argc, char **argv)
 
 	if (argc == 3) {
 		/* redirect stdout to destination file */
+		fd_dst = open(argv[2], O_RDWR | O_CREAT, 0644);
+		if (fd_dst < 0) {
+			printf("[open] failed\n");
+			return ERRO;
+		}
 
+		/*
+		rc = ftruncate(fd_dst, 0);
+		if (rc < 0) {
+			printf("[ftruncate] failed");
+			return ERRO;
+		}
+
+		rc = lseek(fd_dst, 0, SEEK_SET);
+		if (rc < 0) {
+			printf("[lseek] failed");
+			return ERRO;
+		}
+		*/
+
+		dup2(fd_dst, STDIN_FILENO);
+	} else {
+		fd_dst = STDOUT_FILENO;
 	}
 
 	/* read from file and print to stdout
 	 * use _only_ read and write functions
-     * for writing to output use write(STDOUT_FILENO, buffer, bytesRead);
+	 * for writing to output use write(STDOUT_FILENO, buffer, bytesRead);
 	 */
 	while (bytesRead != 0) {
 		bytesRead = xread(fd_src, buffer, BUFSIZE);
-		xwrite(STDOUT_FILENO, buffer, bytesRead);
+		xwrite(fd_dst, buffer, bytesRead);
 	}
-
-    /* TODO 3 - Change the I/O strategy and implement xread/xwrite. These
-     * functions attempt to read _exactly_ the size provided as parameter.
-     */
 
 	/* close file */
 	close(fd_src);
